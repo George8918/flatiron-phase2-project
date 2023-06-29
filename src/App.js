ca7472ccf01b94cf5faf70fbdc38085cc08c4016
George <GZheng8918@gmail.com>
2023-06-28 23:31:43 -0700

    establish Cuisine page components and import it

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {BroswerRouter as Router, Route,Switch,Link} from 'react-router-dom';
+import Home from './components/Home';
+import 
 import './App.css';
 
 function App() {
