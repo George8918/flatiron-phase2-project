c4abc8b56613cba2af29ed2541db87eb70273117
George <GZheng8918@gmail.com>
2023-06-28 23:48:34 -0700

    establish basic router path

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import React from 'react'
 import {BroswerRouter as Router, Route,Switch,Link} from 'react-router-dom';
 import Home from './components/Home';
-import 
+import CuisinePage from './components/CuisinePage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Comfirming this react page is working</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to ="/">Home</Link>
+          </li>
+          <li>
+            <Link to = "/cuisine">Cuisine</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/" component ={Home}/>
+        <Route path="/cuisine" component={CuisinePage} />
+      </Switch>
+    </Router>
   );
 }
+
 
 export default App;
