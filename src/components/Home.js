a18b897b2b43516330ff3b83a32d3b0e2fc8fab0
George <GZheng8918@gmail.com>
2023-06-29 23:36:57 -0700

    home component

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <p>Welcome to the Cuisine and Cultural Photos App!</p>
+        </div>
+    )
+}
+
+export default Home;
