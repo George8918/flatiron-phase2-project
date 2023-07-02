6e32799972ff223b26d3590f10b94a3c6c114c9b
George <GZheng8918@gmail.com>
2023-07-02 12:12:44 -0700

    fetch data from API

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,3 +1,12 @@
 const fs = require('fs');
 const fetch = require('node-fetch');
 
+fetch('https://pixabay.com/api/?key=YOUR_API_KEY&q=cuisine&per_page=10')
+  .then((response) => response.json())
+  .then((data) => {
+    if (data.hits) { 
+        fs.readFile('db.json', 'utf8', (err, fileData) => {
+            if (err) {
+              console.error('Error reading db.json:', err);
+              return;
+            }
